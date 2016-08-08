ce1ba294cb16466a282fb93d24c3704fa1011cc8
gerry <yuanguohua_7818@163.com>
2016-08-08 23:23:06 +0800

    初始化脚本修改

--- common-user-server/DB/create/init.sql
+++ common-user-server/DB/create/init.sql
@@ -1,15 +1,12 @@
-CREATE TABLE `common_send_sms` (
-	`id` INT(20) NOT NULL AUTO_INCREMENT COMMENT 'id自增主键',
-	`phone` VARCHAR(25) NOT NULL COMMENT '手机号',
-	`content` VARCHAR(255) NOT NULL COMMENT '短信内容',
-	`remark` VARCHAR(255) NULL DEFAULT NULL COMMENT '返回内容',
-	`supplier` VARCHAR(50) NOT NULL COMMENT '短信供应商',
-	`type` TINYINT(4) NOT NULL DEFAULT '0' COMMENT '发送类型，0：注册',
-	`status` TINYINT(2) NOT NULL DEFAULT '1' COMMENT '1：成功，2：失败',
-	`create_time` DATETIME NOT NULL COMMENT '创建时间',
-	PRIMARY KEY (`id`)
-)
-COMMENT='发送短信'
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-;
+CREATE TABLE `common_user` (
+  `id` bigint(64) unsigned NOT NULL,
+  `name` varchar(45) NOT NULL DEFAULT '',
+  `phone` varchar(45) NOT NULL DEFAULT '',
+  `nick_name` varchar(45) NOT NULL DEFAULT '',
+  `password` varchar(45) NOT NULL DEFAULT '',
+  `gender` tinyint(2) NOT NULL DEFAULT '0' COMMENT '0：男，1：女',
+  `status` tinyint(2) NOT NULL DEFAULT '0' COMMENT '0：正常，1：删除',
+  `create_time` bigint(40) NOT NULL,
+  `update_time` bigint(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
